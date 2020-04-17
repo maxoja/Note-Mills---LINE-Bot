5b1c7589d826cf0dcf2704b0656090e9b0c945ef
maxoja <tawan.tpptnk@gmail.com>
2020-04-17 21:38:06 +0100

    Cleanup loggings

--- start.py
+++ start.py
@@ -34,64 +34,45 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print('=> handling event')
+    print('========>')
+    print('Handling Event:')
     print(event)
-    print('message')
+    print()
+
+    print('Attached Message:')
     print(event.message)
+    print()
 
     tags = "".join((char if char.isalpha() else " ") for char in event.message.text).split()
-    print('extracted tags')
+    print('Extracted Note Tags:')
     print(tags)
+    print()
 
     notes = get_notes_by_tags(evernote, tags)
-    print('retrieved',len(notes),'notes')
+    print('Retrieved',len(notes),' Notes')
+    print()
 
     if len(notes) == 0:
         send_message(line, "Please select an existing tag")
     else:
+        print('Shuffling retrieved notes ...')
         shuffle(notes)
-        print('picked note')
-        print(str(notes[0])[:30])
-        reply_text = note_to_text(notes[0])
-        print('text in note')
-        print(reply_text[:30])
+        picked_note = notes[0]
+        reply_text = note_to_text(picked_note)
+        print('Picked note has content =',reply_text[:30],'...')
         send_message(line, reply_text)
+    
+    print('<========')
 
 @app.route("/", methods=['GET'])
 def home():
-    return 'Welcome Home'
+    return 'Welcome to the Home of Note Mills'
 
 @app.route("/webhook", methods=['GET', 'POST'])
 def webhook():
     if request.method == 'POST':
         return 'OK'
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     print('=> handling event')
-#     print(event)
-#     print('message')
-#     print(event.message)
-#     reply_text = 'Papuanewgini'
-#     tags = event.message.text.replace(' ','').split(',')
-#     notes = get_notes_by_tags(evernote, tags)
-#     print('retrieved',len(notes),'notes')
-# 
-    # if len(notes) == 0:
-    #     reply_text = 'There is no note with tag ' + str(tags)
-    # else:
-    #     shuffle(notes)
-    #     print('after shuffle')
-    #     print('first note')
-    #     print(str(notes[0])[:30])
-    #     reply_text = note_to_text(notes[0])
-    #     print('text in note')
-    #     print(reply_text[:30])
-# 
-    # send_message(line, reply_text)
-# 
-    # print('handler returning OK')
-    # return 'OK'
 if __name__ == "__main__":
     send_message(line, "Bot server has started")
     app.run()
