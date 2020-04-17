1452c1ebb94d931bb94393a35d449f8e5140fd2e
maxoja <tawan.tpptnk@gmail.com>
2020-04-17 14:09:20 +0100

    Fix unrenamed variable access

--- start.py
+++ start.py
@@ -35,7 +35,7 @@ def home():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
+    line.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
