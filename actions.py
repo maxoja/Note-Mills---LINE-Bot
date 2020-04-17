c23f6519acaa0e5341794cb9689c47f214585b1b
maxoja <tawan.tpptnk@gmail.com>
2020-04-17 01:58:08 +0100

    Function for finding notes by tags

--- actions.py
+++ actions.py
@@ -4,9 +4,14 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 import evernote.edam.userstore.constants as UserStoreConstants
 import evernote.edam.type.ttypes as Types
+from evernote.edam.notestore import NoteStore
 from evernote.api.client import EvernoteClient
 import hashlib, binascii
 import configs
+
+import math
+
+VERY_LARGE_INT = 999999999
 
 def init_line_client():
     return LineBotApi(configs.CHANNEL_ACCESS_TOKEN)
@@ -23,3 +28,17 @@ def init_evernote_client():
         print('Incompatible protocol, terminate application')
         exit(1)
     return evernote
+
+def get_notes_by_tags(client, tags=[]):
+    note_store = client.get_note_store()
+
+    tag_objs = note_store.listTags(configs.EVERNOTE_SANDBOX_ACCESS_TOKEN)
+
+    search_filter = NoteStore.NoteFilter()
+    search_filter.inactive = False
+    search_filter.tagGuids = []
+    [search_filter.tagGuids.append(tag.guid) for tag in tag_objs if tag.name in tags]
+    
+    result = note_store.findNotes(configs.EVERNOTE_SANDBOX_ACCESS_TOKEN, search_filter, 0, VERY_LARGE_INT)
+    notes = result.notes
+    return [note_store.getNote(configs.EVERNOTE_SANDBOX_ACCESS_TOKEN,meta.guid, True, True, True, True) for meta in notes]
--- start.py
+++ start.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     line = init_line_client()
     evernote = init_evernote_client()
     
+    tagged_notes = get_notes_by_tags(evernote, ['try'])
